a2db44ca8f1da6161cf1137ec8c4c69a0b5d9514
Keegan Lawley <keeganlk.7@gmail.com>
2019-02-09 00:01:51 -0600

    Angular implementation

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+  title = "roboboat";
+  items: Observable<any[]>;
+  constructor(db: AngularFirestore) {
+    this.items = db.collection("users").valueChanges();
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB1B6DMiAYU7yqtAjK_AHJ7HujwlpqhjVA",
+    authDomain: "fsuroboboat.firebaseapp.com",
+    databaseURL: "https://fsuroboboat.firebaseio.com",
+    projectId: "fsuroboboat",
+    storageBucket: "fsuroboboat.appspot.com",
+    messagingSenderId: "386517486113" 
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
